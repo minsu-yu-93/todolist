ae610343feaeecf1334d35ed17720394fd8b9310
minsuyu <minsuyu@estsecurity.com>
2025-06-11 22:33:29 +0900

    cmd_line mode 기초 작업업

---
+++ main/cmd_mode.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func cmd_line_run() {
+	fmt.Println("cmd_mode start")
+}
--- main/main.go
+++ main/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello Word")
+	cmd_line := flag.Bool("c", false, "CLI mode")
+	server := flag.Bool("s", false, "Server mode")
+
+	flag.Parse()
+
+	if *cmd_line {
+		fmt.Println("Start Command line mode")
+		cmd_line_run()
+	} else if *server {
+		fmt.Println("Start Server mode")
+	}
 }
